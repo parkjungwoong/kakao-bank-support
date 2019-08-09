dcacf76b4e0deddb4d72231a3d4566a9328068da
jungwoongpark <myuoong@gmail.com>
2019-08-09 16:59:02 +0900

    first commit

---
+++ src/main/java/com/kakaopay/banksupport/BankSupportApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.banksupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSupportApplication.class, args);
+	}
+
+}
