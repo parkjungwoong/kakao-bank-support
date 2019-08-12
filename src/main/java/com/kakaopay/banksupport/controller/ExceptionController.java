d82e7363d3b9f7fcba74d36f8a51c8b3b1926c69
jungwoongpark <myuoong@gmail.com>
2019-08-12 17:41:43 +0900

    Exception 컨트롤러 추가

---
+++ src/main/java/com/kakaopay/banksupport/controller/ExceptionController.java
@@ -0,0 +1,29 @@
+package com.kakaopay.banksupport.controller;
+
+import com.kakaopay.banksupport.common.constant.ErrorCode;
+import com.kakaopay.banksupport.common.exception.ComException;
+import com.kakaopay.banksupport.dto.ComDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@ControllerAdvice
+@RestController
+public class ExceptionController {
+
+    @ExceptionHandler(ComException.class)
+    public ComDTO sendComErrorMsg(ComException e) {
+        //todo:
+        return new ComDTO(ErrorCode.E999);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ComDTO sendErrorMsg(Exception e) {
+        //todo:
+        return null;
+    }
+
+
+}
