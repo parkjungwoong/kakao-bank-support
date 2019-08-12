3f9865d7cd1bf1cf52cdda1ac7df60e54be9fdbd
jungwoongpark <myuoong@gmail.com>
2019-08-12 17:40:40 +0900

    Csv DTO 데이터 타입 수정

--- src/main/java/com/kakaopay/banksupport/dto/CsvDTO.java
+++ src/main/java/com/kakaopay/banksupport/dto/CsvDTO.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class CsvDTO {
     @Getter
-    private List<String> row;
+    private List<List<String>> row;
 
-    private CsvDTO(List<String> row) {
+    private CsvDTO(List<List<String>> row) {
         this.row = row;
     }
 
-    public static CsvDTO CreateCsvDTO(List<String> row) {
+    public static CsvDTO CreateCsvDTO(List<List<String>> row) {
         return new CsvDTO(row);
     }
 
-    public String[] getRowData(int index) {
-        return row.get(index).split(",");
+    public List<String> getRowData(int index) {
+        return row.get(index);
     }
 }
