015baa73528fccdc27ecaf58a0f25b7eb2bc37f8
jungwoongpark <myuoong@gmail.com>
2019-08-16 03:58:13 +0900

    설정값 수정

--- src/main/java/com/kakaopay/banksupport/BankSupportApplication.java
+++ src/main/java/com/kakaopay/banksupport/BankSupportApplication.java
@@ -1,5 +1,6 @@
 package com.kakaopay.banksupport;
 
+import com.kakaopay.banksupport.config.ComSettings;
 import com.kakaopay.banksupport.service.BankService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,24 +8,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
 
 @Slf4j
 @SpringBootApplication
 public class BankSupportApplication {
 
 	private BankService bankService;
+	private ComSettings comSettings;
+	private ResourceLoader resourceLoader;
+	private static String filePath;
 
 	@Autowired
-	public BankSupportApplication(BankService bankService) {
+	public BankSupportApplication(BankService bankService, ComSettings comSet, ResourceLoader resourceLoader) {
 		this.bankService = bankService;
+		this.comSettings = comSet;
+		this.resourceLoader = resourceLoader;
 	}
 
 	public static void main(String[] args) {
+		if(args.length > 0) filePath = args[0];
 		SpringApplication.run(BankSupportApplication.class, args);
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void EventListenerExecute(){
-		//bankService.setUpInitData();
+	public void EventListenerExecute() {
+		if(filePath != null && filePath.trim().length() > 0) comSettings.setInitDataFilePath(filePath);
+
+		log.info("Load File => [{}]" ,comSettings.getInitDataFilePath());
+		bankService.setUpInitData();
 	}
 }
