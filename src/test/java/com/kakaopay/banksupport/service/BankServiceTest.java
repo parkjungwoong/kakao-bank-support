5acca9278648a97527df1e861fd9f8a669affad2
jungwoongpark <myuoong@gmail.com>
2019-08-16 04:02:06 +0900

    서비스 테스트 코드 추가

--- src/test/java/com/kakaopay/banksupport/service/BankServiceTest.java
+++ src/test/java/com/kakaopay/banksupport/service/BankServiceTest.java
@@ -3,8 +3,9 @@ package com.kakaopay.banksupport.service;
 import com.kakaopay.banksupport.common.CommonUtils;
 import com.kakaopay.banksupport.config.ComSettings;
 import com.kakaopay.banksupport.dto.CsvDTO;
+import com.kakaopay.banksupport.dto.SearchDTO;
+import com.kakaopay.banksupport.dto.res.LbsResDTO;
 import com.kakaopay.banksupport.helper.db.TestMapper;
-import com.kakaopay.banksupport.mapper.BankMapper;
 import com.kakaopay.banksupport.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
@@ -14,7 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -35,46 +38,32 @@ public class BankServiceTest {
     @Before
     public void before() {
         /*--------------- given ---------------*/
-        comSettings.setInitDataFilePath(testCsvFilePath); //해당 설정값 경로의 csv 파일을 읽음
-        testCsvData = CommonUtils.readCsvFile(testCsvFilePath);
+        //comSettings.setInitDataFilePath(testCsvFilePath); //해당 설정값 경로의 csv 파일을 읽음
+        //testCsvData = CommonUtils.readCsvFile(testCsvFilePath);
 
         /*--------------- when ---------------*/
-        service.setUpInitData();
+        //service.setUpInitData();
     }
 
     @Test
     public void 지원_정보_원장_데이터_검사() {
-        List<List<String>> row = testCsvData.getRow();
         /*--------------- then ---------------*/
         List<LocalBankSupport> 지원정보_원장_전체 = testMapper.지원정보_원장_전체();
 
-        assertThat(지원정보_원장_전체.size(), is(4));
         for(LocalBankSupport lbs : 지원정보_원장_전체) {
             assertThat(lbs.getLbsId(), notNullValue());
         }
     }
 
     @Test
-    public void 기준정보_데이터_세팅() {
+    public void 검색옵션이_없으며_전체_검색() {
+        /*--------------- given ---------------*/
+        SearchDTO searchDTO = new SearchDTO();
+
+        /*--------------- when ---------------*/
+        LbsResDTO search = service.search(searchDTO);
+
         /*--------------- then ---------------*/
-        List<LocalBankSupport> 지원정보_원장_전체 = testMapper.지원정보_원장_전체();
-        assertThat(지원정보_원장_전체.size(), is(4));
-
-        List<LocalBankSupportDtl> 지원_정보_상세_전체 = testMapper.지원_정보_상세_전체();
-        assertThat(지원_정보_상세_전체.size(), is(4));
-
-        //용도 코드
-        List<UsageCode> 용도_코드_전체 = testMapper.용도_코드_전체();
-        assertThat(용도_코드_전체.size(), is(2));
-
-        List<UsageOpt> 용도_옵션_전체 = testMapper.용도_옵션_전체();
-        assertThat(용도_옵션_전체.size(), is(6));
-
-        //지자체 정보
-        List<RegionMas> 지자체_정보_전체 = testMapper.지자체_정보_전체();
-        assertThat(지자체_정보_전체.size(), is(4));
-
-        //todo: 지역정보 개발 완료되면 테스트 넣기
-        List<RegionMap> 지역_정보_전체 = testMapper.지역_정보_전체();
+        assertThat(search.getList(), notNullValue());
     }
 }
